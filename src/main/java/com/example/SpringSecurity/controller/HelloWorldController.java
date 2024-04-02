56f54f900173a1426bba0b445f8c90212ab174ef
“Ramesh <rameshkumar6plus@gmail.com>
2024-04-02 16:18:31 +0530

    security code

---
+++ src/main/java/com/example/SpringSecurity/controller/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.example.SpringSecurity.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpringSecurity.controller.service.DatabaseService;
+
+@RestController
+public class HelloWorldController {
+
+	@Autowired
+	private DatabaseService dataBaseService;
+
+	@GetMapping("/helloworld")
+	public String getHelloWorldMessage() {
+		return "hello world";
+	}
+
+	@GetMapping("/profile")
+	public Boolean getProfile() {
+
+		return dataBaseService.performDatabaseOperation();
+	}
+
+}
